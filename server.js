31a7ec07866d1fc046adb18ba6e6073b2c82b2a3
jmezzalira <jmezzalira@hotmail.com>
2023-04-09 20:22:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello world with express!!!'))
+app.get('/', (req,res) => res.send('Hello world with express'))
 
 app.listen(3000);
 
